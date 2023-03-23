52ea2b74f96db5814fe68be926b1368985fed091
DaryaGolovko <golovko.darya.al@gmail.com>
2023-03-24 02:29:47 +0300

    trying 1st and 2nd task

---
+++ lab2/lab2.sql
@@ -0,0 +1,94 @@
+--1st task
+
+CREATE TABLE Students (
+    id NUMBER,
+    name VARCHAR2(20),
+    group_id NUMBER
+);
+  
+CREATE TABLE Groups (
+    id NUMBER,
+    name VARCHAR2(20),
+    c_val NUMBER
+);
+
+--2nd
+--check unique group.name
+CREATE OR REPLACE TRIGGER CheckUniqueGroupName
+    BEFORE INSERT OR UPDATE OF NAME ON Groups 
+    FOR EACH ROW 
+DECLARE 
+    num NUMBER;
+BEGIN
+    SELECT COUNT(*) 
+    INTO num 
+    FROM Groups 
+    WHERE name = :NEW.name;
+                
+    IF num > 0 THEN    
+        RAISE VALUE_ERROR; 
+    END IF;
+END;
+
+--check unique id
+CREATE OR REPLACE TRIGGER CheckIdStudents
+    BEFORE INSERT ON Students
+    FOR EACH ROW
+DECLARE
+    num NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO num
+    FROM Students
+    WHERE id = :NEW.id;
+  
+    IF num > 0 THEN
+        RAISE VALUE_ERROR;
+    END IF;
+END;
+
+CREATE OR REPLACE TRIGGER CheckIdGroups
+    BEFORE INSERT ON Groups
+    FOR EACH ROW
+DECLARE
+    num NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO num
+    FROM Groups
+    WHERE id = :NEW.id;
+  
+    IF num > 0 THEN
+        RAISE VALUE_ERROR;
+    END IF;
+END;
+
+--autoincrement
+--helpers
+CREATE SEQUENCE GroupsSequence
+    START WITH 1 
+    INCREMENT BY 1;
+
+CREATE SEQUENCE StudentsSequence
+    START WITH 1 
+    INCREMENT BY 1;
+
+--task
+CREATE OR REPLACE TRIGGER IncrIdStudents
+    BEFORE INSERT ON Students
+    FOR EACH ROW
+BEGIN
+    IF :NEW.id IS NULL THEN
+        :NEW.id := StudentsSequence.NEXTVAL;
+    END IF;
+END;
+
+CREATE OR REPLACE TRIGGER IncrIdGroups
+    BEFORE INSERT ON Groups
+    FOR EACH ROW
+BEGIN
+    IF :NEW.id IS NULL THEN
+        :NEW.id := GroupsSequence.NEXTVAL;
+    END IF;
+END;
+
