b2d54435a09bb6247c8acc648d6169ca213f4476
DaryaGolovko <golovko.darya.al@gmail.com>
2023-03-24 03:24:28 +0300

    tests and helpers

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -1,3 +1,19 @@
+--helpers general
+DROP TABLE Students;
+DROP TABLE Groups;
+
+SELECT * FROM Students;
+SELECT * FROM Groups;
+SELECT * FROM Logs;
+
+--helpers test triggers
+ALTER TABLE Groups DISABLE ALL TRIGGERS;
+ALTER TABLE Students DISABLE ALL TRIGGERS;
+
+ALTER TRIGGER generate_groups_id ENABLE;
+ALTER TRIGGER CheckIdGroups ENABLE;
+ALTER TRIGGER CheckIdStudents ENABLE; 
+
 --1st task
 
 CREATE TABLE Students (
@@ -13,6 +29,17 @@ CREATE TABLE Groups (
 );
 
 --2nd
+--helpers
+INSERT INTO Groups(name) VALUES('1');
+INSERT INTO Groups(name) VALUES('2');
+INSERT INTO Groups(name) VALUES('3');
+INSERT INTO Groups(name) VALUES('4');
+INSERT INTO Groups(name) VALUES('5');
+
+INSERT INTO Students(name, group_id) VALUES('A', 1);
+INSERT INTO Students(name, group_id) VALUES('B', 2);
+INSERT INTO Students(name, group_id) VALUES('C', 3);
+
 --check unique group.name
 CREATE OR REPLACE TRIGGER CheckUniqueGroupName
     BEFORE INSERT OR UPDATE OF NAME ON Groups 
@@ -29,6 +56,11 @@ BEGIN
         RAISE VALUE_ERROR; 
     END IF;
 END;
+
+--tests
+ALTER TRIGGER CheckIdGroups ENABLE;
+INSERT INTO Groups(name) VALUES('1');
+ALTER TABLE Groups DISABLE ALL TRIGGERS;
 
 --check unique id
 CREATE OR REPLACE TRIGGER CheckIdStudents
@@ -63,17 +95,31 @@ BEGIN
     END IF;
 END;
 
+--tests
+select * from students;
+
+ALTER TRIGGER CheckIdGroups ENABLE;
+ALTER TRIGGER CheckIdStudents ENABLE;
+
+INSERT INTO Groups(id, name) VALUES(1, '10');
+select * from GROUPS;
+
+ALTER TRIGGER CheckIdGroups DISABLE;
+ALTER TRIGGER CheckIdStudents DISABLE;
+
 --autoincrement
---helpers
+--helpers sequence
 CREATE SEQUENCE GroupsSequence
     START WITH 1 
     INCREMENT BY 1;
 
-CREATE SEQUENCE StudentsSequence
+CREATE  SEQUENCE StudentsSequence
     START WITH 1 
     INCREMENT BY 1;
 
---task
+--drop sequence GROUPSSEQUENCE;
+
+
 CREATE OR REPLACE TRIGGER IncrIdStudents
     BEFORE INSERT ON Students
     FOR EACH ROW
@@ -99,6 +145,13 @@ BEGIN
     DELETE FROM Students WHERE group_id = :OLD.id;
 END;
 
---4th logs
+--tests
+DELETE FROM Groups WHERE id = 2;
+select * from GROUPS;
+select * from STUDENTS;
 
+
+--4th logs
+--???
+--5th logs restoring - later
 
